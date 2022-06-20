e85af65357c2cf4872c4fefa0d53e19b1773b60e
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-06-20 13:03:41 +0300

    Add dropdown javaScript and events

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const active_item = document.querySelectorAll('.navbar-item ul li a');
 const toggle = document.getElementById('toggle');
-const dropdown = document.querySelector('.fa-caret-down');
-
+const dropdown = document.querySelector('a[href="#dropdown"]');
+const dropdown_icon = document.querySelector('.fa-caret-down');
+const sub_menu = document.querySelector('.sub-menu');
+const sub_menu_items = document.querySelectorAll('.sub-menu ul li a');
 
 active_item.forEach((ele) => {
     ele.onclick = function() {
@@ -9,15 +11,34 @@ active_item.forEach((ele) => {
             ele.classList.remove('active');
         });
         this.classList.add('active');
+        if (ele == dropdown) {
+            if (dropdown_icon.classList.contains('active')) {
+                dropdown_icon.classList.remove('active');
+                sub_menu.style.display = 'none';
+            }else {
+                dropdown_icon.classList.add('active');
+                sub_menu.style.display = 'block';
+            }
+        } else {
+            dropdown_icon.classList.remove('active');
+            sub_menu.style.display = 'none';
+        }
+
     };
 });
 
 
-toggle.onclick = function() {
-    toggle.classList.toggle('active');
-};
 
+sub_menu_items.forEach((ele) => {
+    ele.addEventListener("click", () => {
+        dropdown_icon.classList.remove('active');
+        sub_menu.style.display = 'none';
+        
+    });
 
-dropdown.addEventListener("click", () => {
-    dropdown.classList.add('active');
 });
+
+
+// toggle.addEventListener("click", () => {
+//     toggle.classList.toggle('active');
+// });
